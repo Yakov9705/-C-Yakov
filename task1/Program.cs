0226c579da70200d02190c3a091e67dd98b70a78
Denis B. <Deniscapital@mail.ru>
2024-04-03 18:18:27 +0300

    create D C B  code

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,9 @@
-﻿console.writeline("hello world");
+﻿int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
